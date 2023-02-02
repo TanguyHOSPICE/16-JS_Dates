9259ac0f5662c7fceb5a982572a7e45c63b861fd
Ht <htcvhs971@gmail.com>
2023-02-02 12:48:45 +0100

    Defi Dates init

---
+++ script.js
@@ -0,0 +1,10 @@
+/**
+ * Dans cet exercice, nous voulons gérer les dates des Jeux Olympiques d'été de 2028 à Los Angeles, qui commenceront le 21 juillet 2028 et qui dureront 16 jours. Nous voulons donc afficher toutes les informations utiles concernant cet événement, comme la date de la cérémonie d'ouverture (date de début ), la date de la cérémonie de clôture (date de fin), ainsi que le nombre de jours restants entre la date actuelle et la date de début de l'événement.
+
+Il nous faut donc :
+1-Créer la date initiale (dateStart).
+2-Calculer et créer la date de fin (dateEnd).
+3-Afficher dateStart et dateEnd au format long (ex : samedi 29 février 2020) dans la console.
+4-Créer la date courante currentDate et calculer la différence (result) avec la date de début, puis convertir cette différence en nombre de jours.
+5-Afficher la date courante au format long (ex : samedi 29 février 2020) dans la console et afficher le nombre de jours de différence dans la console.
+ */
